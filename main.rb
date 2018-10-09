fa6d3c0e2ff8954719ee4f3696fc65bb94b62824
Janaki Patel <40279839+jankilighthouse@users.noreply.github.com>
2018-10-08 18:23:36 -0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -19,10 +19,10 @@ end
 
 
 loop do
-turn(P1)
-break puts "Player 2 wins with a score of #{P2.life}/3" if (P1.gameover)
-turn(P2)
-break puts "Player 1 wins with a score of #{P1.life}/3" if (P2.gameover)
-puts "-----NEW TURN-----"
+  turn(P1)
+  break puts "Player 2 wins with a score of #{P2.life}/3" if (P1.gameover)
+  turn(P2)
+  break puts "Player 1 wins with a score of #{P1.life}/3" if (P2.gameover)
+  puts "-----NEW TURN-----"
 end
 puts "-----GAME OVER-----"
